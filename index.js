1bb7c73d472cf2b5023cadf3ba2f9f189a796c9b
JulioAtuesta <jcatuestav@uqvirtual.edu.co>
2023-09-23 13:31:35 -0500

    task checked, cambio de estilo css al clickear

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
+(() =>{
 function addTask(evento){
     evento.preventDefault();
     const inputText = document.getElementById("todo__front-input");
+    if(inputText.value == ''){return(1)};
     const tasksUl = document.getElementById("todo__front-list");
     const newTask = createTask(inputText); 
-            /*
-        ?revisar, si el content esta vacio no se añade la task 
-        */
     console.log(newTask);
     inputText.value = "";
-    tasksUl.appendChild(newTask);    
+    tasksUl.appendChild(newTask); 
+    _isChecked();
 };
 
 function createTask(contenido){
@@ -33,25 +33,49 @@ function createTask(contenido){
 };
 
 function _isChecked(){
-    for (let i of checked){
-        if (checked[i]==check){
-            /* 
-            ?ya no se lo que hago, la idea es escuchar al chechek se puede? es un imput
-            ?luego de eso llamar a la funcion que revisa la cadena, es necesario revisarla toda?
-            ?si el elemento esta checked cambia el style y subraya el texto de la task*/
-        }
+    const checked = document.getElementsByClassName("checkTask");
+    for (const i of checked){
+        console.log(checked);
+        console.log(i);
+        i.addEventListener("click", taskDone =()=>{
+            if(i.checked){
+                console.log(i.parentNode);
+                i.parentNode.style.cssText="text-decoration:line-through;";
+            }else{
+                i.parentNode.style.cssText="text-decoration:none;";
+            }
+        });  
+        /*
+//esto no funciona no ejecuta en "tiempo real" el llamado al taskdone el listener si lo hace
+            taskDone;}*/ 
     }
+};
+
+/*
+function taskDone(){
+    console.log('llamado');
+    if(i.checked){
+        console.log('done');
+    }
+//    text-decoration:line-through
+if(i.checked){
+    console.log(`la tarea ${i + 1} is checked`);
+}else{ 
+    console.log(`la tarea ${i + 1} not checked`);
 }
+};
+*/
 
 function main(){
     const add_button = document.getElementById("todo__front-button");
     add_button.addEventListener("click",addTask);
-    const checked = document.getElementsByClassName("checkTask");
-    checked.addEventListener("click",_isChecked);    
 };
 
 window.addEventListener("load",main);
-    /*
+  
+})();//IIFE por seguridad, las funciones de adentro no estan al alcance del user
+
+  /*
     !importanto: 
     ?si una funcion no tiene parametros se llama 
     !SIN parentesis 
